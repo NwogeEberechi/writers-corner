0200ea8f99c839162b63602425ee63f915814ec6
NwogeEberechi <eberechinwoge@gmail.com>
2018-09-18 08:26:37 +0000

    feature(create-article): Implement article creation 1. add routes for posting, deleting, upgating and getting articles 2. Create article model 3. add serializer files

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+
+from .views import Index, ArticleList, ArticleDetail
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', Index.as_view(), name='index'),
+    path('articles/', ArticleList.as_view(), name='news'),
+    path('articles/<int:pk>/', ArticleDetail.as_view(), name='news_details'),
 ]
--- newsapi/views.py
+++ newsapi/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework import generics
+from rest_framework.views import APIView
+
+from .models import Article
+from  .serializers import ArticleSerializer
 
 # Create your views here.
+class Index(APIView):
+    def get(self, request):
+        data = 'Hello World'
+        return Response(data)
 
-def index(request):
-    return HttpResponse('Hello world')
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
