bbab874f9c903e0494181ffe0bd0bda334396ba0
NwogeEberechi <eberechinwoge@gmail.com>
2018-09-26 13:23:29 +0000

    use viewsets for routing

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
-from .views import Index, ArticleList, ArticleDetail, UserArticle, Users
+from .views import Index, UserArticle, UserViewSet, ArticleViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, base_name="my_users")
+router.register(r'articles', ArticleViewSet, base_name='my_articles')
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('articles/', ArticleList.as_view(), name='news'),
-    path('articles/<int:pk>/', ArticleDetail.as_view(), name='news_details'),
-    path('users/', Users.as_view(), name='users'),
     path('user/<int:pk>/articles/', UserArticle.as_view(), name='user_article'),
-    #path("login", Login.as_view(), name="login"),
-    #path("login", views.obtain_auth_token, name="login"),
 ]
+
+urlpatterns += router.urls
--- newsapi/views.py
+++ newsapi/views.py
@@ -5,26 +5,41 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 
+from rest_framework import viewsets
+from rest_framework.status import HTTP_401_UNAUTHORIZED
+
 from .models import Article
 from  .serializers import ArticleSerializer, UserSerializer
 
 # Create your views here.
 class Index(APIView):
     def get(self, request):
-        data = 'Hello World'
+        data = {"details": "Welcome to Writers-Corner news api"}
         return Response(data)
 
-class ArticleList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        data = UserSerializer(queryset, many=True).data
+        return Response(data)
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        data = UserSerializer(user).data
+        return Response(data)
+
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-class Users(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def destroy(self, request, *args, **kwargs):
+        article = self.get_object()
+        if article.author.id != request.user.id:
+            return Response(
+                data='The article you want to delete belongs to another user! You are only permitted to delete ONLY your article!',
+                status=HTTP_401_UNAUTHORIZED
+                )
+        article.delete()
+        return Response(data={"detail": "Successfully deleted"})
 
 class UserArticle(APIView):
     def get(self, request, pk):
